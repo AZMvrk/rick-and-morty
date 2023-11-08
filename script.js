dae1e494b96fb6a04e569f48ecdeb6a855bf41b7
Zoltan_Mark_Antal <zotyaa95@gmail.com>
2023-11-08 16:39:15 +0100

    skeleton character button

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ const rootElement = document.querySelector("#root")
 
 const fetchUrl = (url) => fetch(url).then(res => res.json())
 
+const skeletonComponent = () => `
+  <div class="characters"></div>
+  <div class="buttons"></div>
+`
+
 const characterComponent = (characterData) => ` 
   <div class="char">
     <img src=${characterData.image}>
@@ -10,18 +15,41 @@ const characterComponent = (characterData) => `
   </div>
 `
 
-const buttonComponent = (id, text) => `<`
+const buttonComponent = (id, text) => `<button id=${id}>${text}</button>`
 
-
+const buttonEventComponent = (id, url) => {
+  const buttonElement = document.querySelector(`#${id}`)
+  buttonElement.addEventListener("click", () => {
+    console.log(`fetch: ${url}`)
+    rootElement.innerHTML = "LOADING..."
+    fetchUrl(url).then(data => makeDomFromData(data, rootElement))
+  })
+}
 
 const makeDomFromData = (data, rootElement) => {
-  const info = data.info // info object
-  const characters = data.results // characters array
+  rootElement.innerHTML = skeletonComponent()
 
-  characters.forEach(character => rootElement.insertAdjacentHTML("beforeend", characterComponent(character)))
+  const charactersElement = document.querySelector(".characters")
+  const buttonsElement = document.querySelector(".buttons") 
+
+  const info = data.info
+  const characters = data.results
+
+  characters.forEach(character => charactersElement.insertAdjacentHTML("beforeend", characterComponent(character)))
+
+  if (info.prev) {
+    buttonsElement.insertAdjacentHTML("beforeend", buttonComponent("prev", "previous"))
+    buttonEventComponent("prev", info.prev)
+  }
+
+  if (info.next) {
+    buttonsElement.insertAdjacentHTML("beforeend", buttonComponent("next", "next"))
+    buttonEventComponent("next", info.next)
+  }
 }
 
 const init = () => {
+  rootElement.innerHTML = "LOADING..."
   fetchUrl("https://rickandmortyapi.com/api/character").then(data => makeDomFromData(data, rootElement))
 }
 
